00255f7c083196178ceb604c3507d5b27968481e
DjowChi Tian <vincenttzq@gamil.com>
2026-09-13 21:08:34 -0600

    to lesson 5 class

---
+++ dmit2015-faces-demo/src/main/java/dmit2015/view/HelloBean.java
@@ -0,0 +1,35 @@
+package dmit2015.view;
+
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+
+@Named
+@RequestScoped
+public class HelloBean {
+    @NotBlank(message = "user input value is required")
+    private  String userInput ;
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public String getMessage(){
+        //return  "Hello, "+userInput;
+        return  String.format("hello, %s",userInput);
+    }
+
+    public String onSubmit(){
+        String message = String.format("Action Hello , %s", userInput);
+        var faceMessage = new FacesMessage (message);
+        FacesContext.getCurrentInstance().addMessage(null,faceMessage);
+        return null;
+    }
+}
---
+++ dmit2015-faces-demo/src/main/java/dmit2015/view/StudentFromBean.java
@@ -0,0 +1,4 @@
+package dmit2015.view;
+
+public class StudentFromBean {
+}
